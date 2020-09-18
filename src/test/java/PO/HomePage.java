e2c73d6b515e6ef0817f901741bf0fc64690f3fe
Antony <antony@mail.com>
2020-09-18 11:48:17 +0300

    added BasePage

--- src/test/java/PO/HomePage.java
+++ src/test/java/PO/HomePage.java
@@ -3,20 +3,15 @@ package test.java.PO;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage {
-
-    WebDriver driver;
-    WebDriverWait wait;
+public class HomePage extends BasePage {
 
     By btnLuck = By.cssSelector(".FPdoLc.tfB0Bf .gNO89b");
     By btnSearch = By.cssSelector(".tfB0Bf .gNO89b");
     By inputSearch = By.name("q");
 
     public HomePage(WebDriver driver) {
-        this.driver = driver;
-        wait = new WebDriverWait(driver, 10);
+        super(driver);
     }
 
     public HomePage isShown() {
@@ -25,12 +20,12 @@ public class HomePage {
         return this;
     }
 
-    public HomePage writeSearchWord(){
+    public HomePage writeSearchWord() {
         driver.findElement(inputSearch).sendKeys("Selenium");
         return this;
     }
 
-    public HomePage clickSearchBtn(){
+    public HomePage clickSearchBtn() {
         wait.until(ExpectedConditions.elementToBeClickable(btnSearch));
         driver.findElement(btnSearch).click();
         return this;
--- src/test/java/PO/SearchPage.java
+++ src/test/java/PO/SearchPage.java
@@ -3,20 +3,13 @@ package test.java.PO;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SearchPage {
-
-    WebDriver driver;
-    WebDriverWait wait;
+public class SearchPage extends BasePage {
 
     private By wikiRef = By.cssSelector("a.q.ruhjFe");
-    private By wikiFooter = By.id("footer");
-    private By wikiHeading = By.cssSelector("h1");
 
     public SearchPage(WebDriver driver) {
-        this.driver = driver;
-        wait = new WebDriverWait(driver, 10);
+        super(driver);
     }
 
     public SearchPage goToWikiPage() {
