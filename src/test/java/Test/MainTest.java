742693c7bbf2e3a967a18b5d2fa33b80ae8ed121
Antony <antony@mail.com>
2020-10-28 17:20:25 +0200

    added BaseMainTest

--- src/test/java/Test/MainTest.java
+++ src/test/java/Test/MainTest.java
@@ -1,30 +1,9 @@
 package test.java.Test;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import test.java.PO.HomePage;
-import test.java.PO.SearchPage;
-import test.java.PO.WikiPage;
 
-public class MainTest {
-
-    private WebDriver driver;
-    private HomePage homePage;
-    private SearchPage searchPage;
-    private WikiPage wikiPage;
-
-    @BeforeMethod
-    public void init() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        homePage = new HomePage(driver);
-        searchPage = new SearchPage(driver);
-        wikiPage = new WikiPage(driver);
-    }
+public class MainTest extends BaseMainTest {
 
     @Test
     public void checkHomePageTitle() {
@@ -60,11 +39,6 @@ public class MainTest {
         searchPage.goToWikiPage();
 
         Assert.assertEquals(wikiPage.getWikiHeading(), "Selenium");
-    }
-
-    @AfterMethod
-    public void close() {
-        driver.quit();
     }
 
 }
